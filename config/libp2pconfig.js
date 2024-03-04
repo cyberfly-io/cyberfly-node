5bc2442967c82d09b915344b04fafc40462aaa8e
cyberfly <info@cyberfly.io>
2024-03-04 15:58:59 +0530

    added public bootstrap nodes

--- config/libp2pconfig.js
+++ config/libp2pconfig.js
@@ -10,7 +10,12 @@ import { bootstrap } from '@libp2p/bootstrap'
 
 export const libp2pOptions = {
     peerDiscovery: [
-      bootstrap({list:["/ip4/170.187.249.181/tcp/31001/p2p/12D3KooWN6UaUYr5QUmQo17KeaPvNBLtdw4irYDt9JYFGRgwGHUn"]})
+      bootstrap({list:["/ip4/170.187.249.181/tcp/31001/p2p/12D3KooWN6UaUYr5QUmQo17KeaPvNBLtdw4irYDt9JYFGRgwGHUn",
+      "/dnsaddr/bootstrap.libp2p.io/ipfs/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa",
+      "/dnsaddr/bootstrap.libp2p.io/ipfs/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN",
+
+
+    ]})
     ],
     addresses: {
       listen: ['/ip4/0.0.0.0/tcp/31001']
