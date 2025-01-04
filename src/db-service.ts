d0bd46b3b403258d2ad81c343216f69835c221d7
cyberfly <info@cyberfly.io>
2025-01-04 21:17:30 +0530

    add datastore

--- src/db-service.ts
+++ src/db-service.ts
@@ -4,21 +4,23 @@ import { createOrbitDB } from '@orbitdb/core'
 import { LevelBlockstore } from 'blockstore-level'
 import { getLibp2pOptions } from './config/libp2pconfig.js'
 import { getIp, loadOrCreatePeerIdAndKeyPair } from './config/utils.js'
+import { LevelDatastore } from "datastore-level"
 
 
 
 const startOrbitDB = async ({sk}) => {
 
   const ip = await getIp()
-  const config = await loadOrCreatePeerIdAndKeyPair('./data/config.json', sk)
-
-  const options =  getLibp2pOptions(ip, config.peerId.toString())
-  const libp2p:any = await createLibp2p({privateKey:config.privateKey, ...options })
-  console.log(libp2p.peerId.toString())
   const directory = './data'
+  const config = await loadOrCreatePeerIdAndKeyPair('./data/config.json', sk)
+  let datastore = new LevelDatastore(`${directory}/ipfs/data`)
+  const options =  getLibp2pOptions(ip, config.peerId.toString())
+  const libp2p:any = await createLibp2p({datastore ,privateKey:config.privateKey, ...options })
+  console.log(libp2p.peerId.toString())
+
   const blockstore = new LevelBlockstore(`${directory}/ipfs/blocks`)
 
-  const ipfs = await createHelia({ libp2p, blockstore  })
+  const ipfs = await createHelia({ libp2p, blockstore, datastore  })
   const orbitdb = await createOrbitDB({ ipfs, directory })
   return {orbitdb, ...config}
 }
